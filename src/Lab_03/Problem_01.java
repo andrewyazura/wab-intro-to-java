9a22d8e6c64d34f0f27df79f2a7ee427873484cf
Andrew Yatsura <andrewyazura203@proton.me>
2023-11-10 16:37:51 +0000

    add solutions for lab 3

---
+++ src/Lab_03/Problem_01.java
@@ -0,0 +1,32 @@
+package Lab_03;
+
+import java.util.Scanner;
+
+public class Problem_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int input = scanner.nextInt();
+
+        if (input < 0) {
+            System.out.println("Error: Input value is less than 0.");
+            System.exit(1);
+        }
+
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 0; i <= input; i++) {
+            if (i % 2 != 0) {
+                sum += i;
+                count++;
+            }
+        }
+
+        System.out.println(
+                "Average of all odd integers between 0 and " + input + " is " + (sum / count));
+
+        scanner.close();
+    }
+}
---
+++ src/Lab_03/Problem_03.java
@@ -0,0 +1,29 @@
+package Lab_03;
+
+public class Problem_03 {
+    public static void main(String[] args) {
+        String[] employee_names =
+                {"John", "Jane", "Jack", "Jill", "James", "Jenny", "Joe", "Jade", "Jade", "Jade"};
+        String[] weekdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+
+        int[][] workload_per_day = new int[10][5];
+
+        // fill workload_per_day with random values from 0 to 10
+        for (int i = 0; i < 5; ++i)
+            for (int j = 0; j < 10; ++j)
+                workload_per_day[j][i] = (int) (Math.random() * 11);
+
+
+        System.out.print("Employee\t");
+        for (int i = 0; i < 10; ++i)
+            System.out.printf("%-6s", employee_names[i]);
+
+        for (int i = 0; i < 5; ++i) {
+            System.out.println();
+            System.out.printf("%-10s\t", weekdays[i]);
+
+            for (int j = 0; j < 10; ++j)
+                System.out.printf("%-6d", workload_per_day[j][i]);
+        }
+    }
+}
