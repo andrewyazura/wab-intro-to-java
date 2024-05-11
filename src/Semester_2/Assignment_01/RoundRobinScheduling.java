f0fd527ae794b3481719ad0b2f09d24b5528373c
Andrew Yatsura <andrewyazura203@proton.me>
2024-05-11 15:07:20 +0200

    add author comments

--- src/Semester_2/Assignment_01/RoundRobinScheduling.java
+++ src/Semester_2/Assignment_01/RoundRobinScheduling.java
@@ -1,3 +1,8 @@
+// Name: Andrii Yatsura
+// Student ID: 19387
+// Course: Algorithms and Data Structures
+// University: Wroclaw Academy of Business
+
 package Semester_2.Assignment_01;
 
 public class RoundRobinScheduling {
